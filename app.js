1f3510c53d5ba821b2b97553abdd97c2a0ea0c05
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2020-10-04 13:52:49 -0400

    updated blog with mongo

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,7 +16,19 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const posts = [];
+
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
 
 app.set("view engine", "ejs");
 
@@ -21,17 +36,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  let truncPosts = [];
-  posts.forEach((post) => {
-    truncPosts.push({
-      title: _.truncate(post.title, { length: 100 }),
-      body: _.truncate(post.body, { length: 100 }),
-      link: `/posts/${_.kebabCase(_.lowerCase(post.title))}`,
+  Post.find({}, (err, posts) => {
+    let truncPosts = [];
+    posts.forEach((post) => {
+      truncPosts.push({
+        title: _.truncate(post.title, { length: 100 }),
+        body: _.truncate(post.body, { length: 100 }),
+        link: `/posts/${_.kebabCase(_.lowerCase(post.title))}`,
+      });
     });
-  });
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: truncPosts,
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: truncPosts,
+    });
   });
 });
 
@@ -46,23 +63,22 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
-    title: req.body.postTitle,
+  const post = new Post({
+    title: _.startCase(req.body.postTitle),
     body: req.body.content,
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  post.save((err) => {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/posts/:title", (req, res) => {
-  for (let i = 0; i < posts.length; i++) {
-    if (req.params.title === _.kebabCase(_.lowerCase(posts[i].title))) {
-      res.render("post", {
-        title: posts[i].title,
-        body: posts[i].body,
-      });
-    }
-  }
+  Post.findOne({ title: _.startCase(req.params.title) }, (err, post) => {
+    console.log(post);
+    res.render("post", post);
+  });
 });
 
 app.listen(3000, function () {
